9dd4172eebf6525ec006eaa42d9c99d129d18726
Ajut Dangol <ajud10gol@gmail.com>
2024-03-01 12:28:53 +0545

    dashjs and themetoggler

--- javascript/dashScript.js
+++ javascript/dashScript.js
@@ -1,2 +1,24 @@
-document.write("helo");
-alert("helo");
+const sideMenu =document.querySelector('aside');
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+
+// show sidebar menu
+menuBtn.addEventListener('click',()=>{
+    sideMenu.style.display = 'block';
+})
+
+// close sidebar menu
+closeBtn.addEventListener('click',()=>{
+    sideMenu.style.display = 'none';
+})
+
+// change theme
+themeToggler.addEventListener('click',()=>{
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+
+})
+
